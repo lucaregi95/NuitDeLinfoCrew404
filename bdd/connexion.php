8c35225b31c7dfbb6a8e1c339529b03c2facaa80
Luca REGI <REGI_Lu@lprs.local>
2025-12-05 00:43:34 +0100

    featbdd en ligne

--- bdd/connexion.php
+++ bdd/connexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $bdd = new PDO("mysql:host=localhost;dbname=nird", "lregi", "lucaregi");
+    $bdd = new PDO("mysql:host=isp.seblemoine.fr:3306;dbname=bdd_nird_crew404", "bdd_nird_crew404", "tkGPdd9Y_");
 }catch (PDOException $e){
     echo "Erreur : ".$e->getMessage();
 }?>
